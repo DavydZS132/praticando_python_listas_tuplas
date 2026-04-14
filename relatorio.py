76d3dffa9f7e69a39558d7a3cda78259cff7c0ba
Davyd Zuquim Silva <dzuquim10@gmail.com>
2026-04-14 20:55:00 -0300

    Ralatorio

---
+++ relatorio.py
@@ -0,0 +1,28 @@
+# Armano trabalha com a gestão de dois estoques de mercadorias que são representados como tuplas. Agora, ele precisa criar um relatório unificado com os produtos dos dois estoques juntos.
+
+# Para ajudá-lo, como você criaria um programa que ler as informações dos estoques e gera um relatório com todos os produtos juntos?
+
+# Exemplo de Entrada:
+
+# Produtos do estoque 1 (separados por vírgula): Arroz, Feijão, Macarrão
+# Produtos do estoque 2 (separados por vírgula): Óleo, Sal, Açúcar
+
+# Saída esperada:
+
+# Estoque combinado:
+# ('Arroz', 'Feijão', 'Macarrão', 'Óleo', 'Sal', 'Açúcar')
+
+# estoque_1 = ('Arroz', 'Feijão', 'Macarrão')
+# estoque_2 = ('Óleo', 'Sal', 'Açúcar')
+
+# estoque_combinado = estoque_1 + estoque_2
+
+# print(*estoque_1, sep=',')
+# print(','.join(map(str, estoque_2)))
+
+# print(f'Estoque combinado:\n {estoque_combinado}')
+
+estoque1 = tuple(input("Produtos do estoque 1 (separados por vírgula): ").split(", "))
+estoque2 = tuple(input("Produtos do estoque 2 (separados por vírgula): ").split(", "))
+estoque_combinado = estoque1 + estoque2  
+print(f"Estoque combinado:\n{estoque_combinado}")
